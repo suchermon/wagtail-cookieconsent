67e6b19361b29938e95e3e61c3c55dbd61590fa8
Mon Sucher <msucher@umn.edu>
2020-04-20 15:14:04 -0500

    Added package_data in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,27 @@
-from setuptools import setup
+import os
+from setuptools import find_packages, setup
 
-setup()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+with open('README.md') as readme:
+    README = readme.read()
+
+
+def get_requirements_tests():
+    with open('requirements.txt') as f:
+        return f.readlines()
+
+
+setup(
+    include_package_data=True,
+    packages=find_packages('.'),
+    package_data={'wagtailcookieconsent': [
+        "templates/wagtailcookieconsent/*.html",
+        "templates/wagtailcookieconsent/*/*.html",
+        "static/*.css",
+        "fixtures/*.json"
+    ]},
+)
